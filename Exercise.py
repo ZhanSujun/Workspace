3eb966e7f75376bf24839e67c129495fe13c6913
Zhan Sujun <zhansujun@hotmail.com>
2020-09-11 17:33:57 +0800

    Exercise.py

---
+++ Exercise.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+print('L[0:3] =', L[0:3])
+print('L[:3] =', L[:3])
+print('L[1:3] =', L[1:3])
+print('L[-2:] =', L[-2:])
+
+R = list(range(100))
+print('R[:10] =', R[:10])
+print('R[-10:] =', R[-10:])
+print('R[10:20] =', R[10:20])
+print('R[:10:2] =', R[:10:2])
+print('R[::5] =', R[::5])
+'''
+'''
+s = (x * x for x in range(5))
+print(s)
+for x in s:
+    print(x)
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+f = fib(10)
+print('fib(10):', f)
+for x in f:
+    print(x)
+
+g = fib(5)
+while 1:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+    '''
+# map()函数的使用方法
+'''
+def f(x):
+    return x * x
+
+print(list(map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])))    
+'''
+
+# filter()函数的使用方法
+'''
+def is_odd(n):
+    return n % 2 == 1
+
+print(list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])))
+
+
+def not_empty(s):
+    return s and s.strip()
+
+print(list(filter(not_empty, ['A', '', 'B', None, 'C', '  '])))
+'''
+'''
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter() # 初始化序列
+    while True:
+        n = next(it) # 返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it) # 构造新序列
+
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
+    '''
+
+# yield运行规则，第一次循环，return；第二次从中断处开始
+'''
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+g = foo()
+print(next(g))
+print("*"*20)
+print(next(g))
+print("*"*20)
+print(next(g))
+print("*"*20)
+print(next(g))
+'''
